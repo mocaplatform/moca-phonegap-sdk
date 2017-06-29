f626999683385d43cd63472481709ed69eb4ba38
Iván González <ivan.gonzalez@me.com>
2017-06-29 16:17:32 +0200

    Android V2+ implement new listeners

--- MOCASDK/src/android/MOCApp.java
+++ MOCASDK/src/android/MOCApp.java
@@ -2,14 +2,7 @@ package com.innoquant.moca.phonegap;
 
 import android.app.Application;
 
-import com.innoquant.moca.MOCA;
-import com.innoquant.moca.MOCAAction;
-import com.innoquant.moca.MOCABeacon;
-import com.innoquant.moca.MOCAConfig;
-import com.innoquant.moca.MOCAPlace;
-import com.innoquant.moca.MOCAProximity;
-import com.innoquant.moca.MOCAProximityService;
-import com.innoquant.moca.MOCAZone;
+import com.innoquant.moca.*;
 import com.innoquant.moca.utils.MLog;
 
 import org.apache.cordova.PluginResult;
@@ -24,16 +17,16 @@ import java.util.List;
  * Created by tomacco on 21/11/15.
  */
 public class MOCApp extends Application implements MOCAProximityService.EventListener, MOCAProximityService.ActionListener{
-
+    
     private HashMap<String, MOCACallbackContext> callbackContextMap = new HashMap<String, MOCACallbackContext>();
-
-
+    
+    
     @Override
     public void onCreate() {
-//        android.os.Debug.waitForDebugger();
-
+        //        android.os.Debug.waitForDebugger();
+        
         super.onCreate();
-
+        
         //Auto Init MOCA SDK
         final String appKey = MOCASharedPrefs.getAppKey(this);
         final String appSecret = MOCASharedPrefs.getAppSecret(this);
@@ -47,7 +40,7 @@ public class MOCApp extends Application implements MOCAProximityService.EventLis
         }
         this.startListeners();
     }
-
+    
     public void startListeners(){
         if(MOCA.initialized()){
             MOCA.getProximityService().setActionListener(this);
@@ -57,50 +50,50 @@ public class MOCApp extends Application implements MOCAProximityService.EventLis
             MLog.wtf("Cannot listen MOCA Events. MOCA is not running.");
         }
     }
-
+    
     public void addCallbackContext(String action, MOCACallbackContext callbackContext){
         this.callbackContextMap.put(action, callbackContext);
         PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
         result.setKeepCallback(true);
         this.callbackContextMap.get(action).sendPluginResult(result);
     }
-
-
-
+    
+    
+    
     /*
-    /* MOCA ActionListener callbacks
+     /* MOCA ActionListener callbacks
      */
-
+    
     @Override
     public boolean displayNotificationAlert(MOCAAction mocaAction, String s) {
         return fireEvent(MOCAConstants.DISPLAY_ALERT, s);
     }
-
+    
     @Override
     public boolean openUrl(MOCAAction mocaAction, String s) {
         return fireEvent(MOCAConstants.OPEN_URL, s);
     }
-
+    
     @Override
     public boolean showHtmlWithString(MOCAAction mocaAction, String s) {
         return fireEvent(MOCAConstants.SHOW_EMBEDDED_HTML, s);
     }
-
+    
     @Override
     public boolean playVideoFromUrl(MOCAAction mocaAction, String s) {
         return fireEvent(MOCAConstants.PLAY_VIDEO_FROM_URL, s);
     }
-
+    
     @Override
     public boolean displayImageFromUrl(MOCAAction mocaAction, String s) {
         return fireEvent(MOCAConstants.IMAGE_FROM_URL, s);
     }
-
+    
     @Override
     public boolean displayPassFromUrl(MOCAAction mocaAction, String s) {
         return fireEvent(MOCAConstants.PASSBOOK_FROM_URL, s);
     }
-
+    
     @Override
     public boolean addTag(MOCAAction mocaAction, String tagName, String tagValue) {
         JSONObject args = new JSONObject();
@@ -113,35 +106,35 @@ public class MOCApp extends Application implements MOCAProximityService.EventLis
         }
         return fireEvent(MOCAConstants.ADD_TAG, args);
     }
-
+    
     @Override
     public boolean playNotificationSound(MOCAAction mocaAction, String s) {
         return fireEvent(MOCAConstants.PLAY_NOTIFICATION_SOUND, s);
     }
-
+    
     @Override
     public boolean performCustomAction(MOCAAction mocaAction, String s) {
         return fireEvent(MOCAConstants.PERFORM_CUSTOM_ACTION, s);
     }
-
-
+    
+    
     /*
-    /* MOCA EventListener callbacks
+     /* MOCA EventListener callbacks
      */
-
+    
     @Override
     public void didEnterRange(MOCABeacon mocaBeacon, MOCAProximity mocaProximity) {
         fireEvent(MOCAConstants.DID_ENTER_RANGE, mocaBeacon);
     }
-
+    
     @Override
     public void didExitRange(MOCABeacon mocaBeacon) {
         fireEvent(MOCAConstants.DID_EXIT_RANGE, mocaBeacon);
     }
-
+    
     @Override
     public void didBeaconProximityChange(MOCABeacon beacon, MOCAProximity prevProximity, MOCAProximity curProximity) {
-
+        
         JSONObject proximityChangeArgs = new JSONObject();
         try {
             JSONObject bkn = Utils.beaconToJSON(beacon);
@@ -153,32 +146,42 @@ public class MOCApp extends Application implements MOCAProximityService.EventLis
             MLog.e(MOCAConstants.BEACON_PROXIMITY_CHANGE + " callback failed. " +  e);
         }
     }
-
+    
     @Override
     public void didEnterPlace(MOCAPlace mocaPlace) {
         fireEvent(MOCAConstants.DID_ENTER_PLACE, mocaPlace);
     }
-
+    
     @Override
     public void didExitPlace(MOCAPlace mocaPlace) {
         fireEvent(MOCAConstants.DID_EXIT_PLACE, mocaPlace);
     }
-
+    
     @Override
     public void didEnterZone(MOCAZone mocaZone) {
         fireEvent(MOCAConstants.DID_ENTER_ZONE, mocaZone);
     }
-
+    
     @Override
     public void didExitZone(MOCAZone mocaZone) {
         fireEvent(MOCAConstants.DID_EXIT_ZONE, mocaZone);
     }
-
+    
+    @Override
+    public void didEnterLabel(MOCALabel mocaLabel, MOCARegion mocaRegion) {
+        
+    }
+    
+    @Override
+    public void didExitLabel(MOCALabel mocaLabel, MOCARegion mocaRegion) {
+        
+    }
+    
     @Override
     public boolean handleCustomTrigger(String s) {
         return false;
     }
-
+    
     @Override
     public void didLoadedBeaconsData(List<MOCABeacon> list) {
         JSONArray beaconList = new JSONArray();
@@ -192,7 +195,7 @@ public class MOCApp extends Application implements MOCAProximityService.EventLis
             MLog.e(MOCAConstants.DID_LOADED_BEACONS_DATA + "callback failed: " + e);
         }
     }
-
+    
     /**
      * fires events (callbacks) in the webview
      * @param eventName is the name of the callback (see MOCA Cordova API)
@@ -200,7 +203,7 @@ public class MOCApp extends Application implements MOCAProximityService.EventLis
      * @return the first argument sent by the webview when suscribing to callback
      * Example moca.
      */
-
+    
     private boolean fireEvent(String eventName, Object data){
         MOCACallbackContext callbackCtx = callbackContextMap.get(eventName);
         if(callbackCtx != null){
@@ -211,7 +214,7 @@ public class MOCApp extends Application implements MOCAProximityService.EventLis
                 result = new PluginResult(PluginResult.Status.OK, event);
                 result.setKeepCallback(true);
                 callbackCtx.sendPluginResult(result);
-
+                
                 //Arguments in callbacks are used to determine if MOCA should
                 //show a Proximity Experience, or only send callbacks.
                 return callbackCtx.getBooleanArg();
@@ -228,7 +231,7 @@ public class MOCApp extends Application implements MOCAProximityService.EventLis
         }
         return false;
     }
-
+    
     //Helper methods
     private JSONObject getJSONFromObject(String eventName, Object data) throws JSONException{
         JSONObject jsonData = new JSONObject();
@@ -247,6 +250,6 @@ public class MOCApp extends Application implements MOCAProximityService.EventLis
         }
         return jsonData;
     }
-
-
+    
+    
 }
